8c7888aca0131077608ecff239d5cbc96b664a12
gnjbs <gnjbs@list.ru>
2015-11-14 23:33:26 +0300

    HW07

--- src/ru/javawebinar/webapp/storage/PathStorage.java
+++ src/ru/javawebinar/webapp/storage/PathStorage.java
@@ -1,9 +1,27 @@
 package ru.javawebinar.webapp.storage;
 
+import ru.javawebinar.webapp.model.Resume;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.nio.file.Path;
+
 /**
  * GKislin
  * 23.10.2015.
  */
 // TODO Optional
-public class PathStorage {
+public class  PathStorage extends AbstractFileStorage<Path>{
+    protected final Path directory;
+
+    @Override
+    protected void write(Resume r, OutputStream os) throws IOException {
+
+    }
+
+    @Override
+    protected Resume read(InputStream is) throws IOException {
+        return null;
+    }
 }
