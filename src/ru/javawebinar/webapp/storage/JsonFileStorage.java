a291bd9bf8923aa8bdf577dcf592a60fd75ad732
gnjbs <gnjbs@list.ru>
2015-11-23 01:14:28 +0300

    HW07 + SerializeFIleStorage - All Tests Passed

--- src/ru/javawebinar/webapp/storage/JsonFileStorage.java
+++ src/ru/javawebinar/webapp/storage/JsonFileStorage.java
@@ -1,9 +1,34 @@
 package ru.javawebinar.webapp.storage;
 
+import ru.javawebinar.webapp.model.Resume;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+
 /**
  * GKislin
  * 30.10.2015.
  */
 // TODO Optional try implements section
-public class JsonFileStorage {
+public class JsonFileStorage extends AbstractFileStorage {
+
+    public JsonFileStorage(String path) {
+        super(path);
+    }
+
+    @Override
+    protected void write(Resume r, OutputStream os) throws IOException {
+
+    }
+
+    @Override
+    protected Resume read(InputStream is) throws IOException {
+        return null;
+    }
+
+    @Override
+    public boolean isSectionSupported() {
+        return false;
+    }
 }
